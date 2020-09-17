ab504e4ee5e12a5e754781db52a0adc4dc998104
Koki Ibukuro <koki.ibukuro@gmail.com>
2020-09-17 10:19:01 +0200

    WIP unity plungin

---
+++ UnityHeadphoneMotionPlugin/Assets/HeadphoneMotionSample/InfoSample.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using HeadphoneMotion;
+
+public class InfoSample : MonoBehaviour
+{
+    [SerializeField] Text infoLabel = null;
+    [SerializeField] Button toggleButton = null;
+
+    StringBuilder sb = new StringBuilder();
+
+    void OnEnable()
+    {
+        toggleButton.onClick.AddListener(OnToggleButtonClick);
+        UpdateInfo();
+    }
+
+    void OnDisable()
+    {
+        toggleButton.onClick.RemoveListener(OnToggleButtonClick);
+        HeadphoneMotionManager.Stop();
+    }
+
+    void OnToggleButtonClick()
+    {
+        if (!HeadphoneMotionManager.IsActive || !HeadphoneMotionManager.IsAvailable)
+        {
+            if (Application.platform == RuntimePlatform.IPhonePlayer)
+            {
+                Debug.LogWarning("Please make sure your AirPods is connected and the firmware is updated");
+            }
+            else
+            {
+                Debug.Log($"HeadphoneMotion is not supported on platform: {Application.platform}");
+            }
+            return;
+        }
+        if (HeadphoneMotionManager.IsConnected)
+        {
+            HeadphoneMotionManager.Stop();
+        }
+        else
+        {
+            HeadphoneMotionManager.Start();
+        }
+    }
+
+    void UpdateInfo()
+    {
+        sb.Clear();
+        sb.AppendLine($"available: {HeadphoneMotionManager.IsAvailable}");
+        sb.AppendLine($"active: {HeadphoneMotionManager.IsActive}");
+        sb.AppendLine($"connected: {HeadphoneMotionManager.IsConnected}");
+        infoLabel.text = sb.ToString();
+    }
+}
--- UnityHeadphoneMotionPlugin/Packages/com.github.asus4.headphonemotion/Runtime/HeadphoneMotionManager.cs
+++ UnityHeadphoneMotionPlugin/Packages/com.github.asus4.headphonemotion/Runtime/HeadphoneMotionManager.cs
@@ -1,60 +1,110 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using Unity.Mathematics;
 
 namespace HeadphoneMotion
 {
-    /*
 
-typedef struct {
-    CMAcceleration userAcceleration;
-    CMQuaternion rotation;
-    CMDeviceMotionSensorLocation location;
-} HeadphoneMotionData;
-typedef void (*UnityHeadphoneMotionCallback)(HeadphoneMotionData motion);
-typedef void (*UnityHeadphoneMotionEventCallback)(BOOL connected);
 
-extern bool _unityHeadphoneDeviceMotionIsAvailable(void);
-extern bool _unityHeadphoneDeviceMotionIsActive(void);
-extern void _unityHeadphoneMotionStart(UnityHeadphoneMotionCallback callback);
-extern void _unityHeadphoneMotionStop(void);
-extern void _unityHeadphoneMotionSetEventCallback(UnityHeadphoneMotionEventCallback callback);
-    */
+    public enum DeviceSensorLocation
+    {
+        Default = 0,
+        HeadphoneLeft = 1,
+        HeadphoneRight = 2,
+    }
 
+    /// <summary>
+    /// typedef struct {
+    ///   CMAcceleration userAcceleration;
+    ///   CMQuaternion rotation;
+    ///   CMDeviceMotionSensorLocation location;
+    /// } HeadphoneMotionData;
+    /// </summary>
+    public struct HeadphoneMotionData
+    {
+        public double3 userAcceleration;
+        public double4 rotation;
+        public DeviceSensorLocation location;
+    }
 
     public class HeadphoneMotionManager
     {
-        public enum SensorLocation
+        // typedef void (*UnityHeadphoneMotionCallback)(HeadphoneMotionData motion);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, SetLastError = true)]
+        private delegate void HeadphoneMotionDelegate(HeadphoneMotionData motionData);
+
+        // typedef void (*UnityHeadphoneMotionEventCallback)(BOOL connected);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, SetLastError = true)]
+        private delegate void HeadphoneMotionEventDelegate(bool connected);
+
+
+        public static bool IsAvailable => _unityHeadphoneDeviceMotionIsAvailable();
+        public static bool IsActive => _unityHeadphoneDeviceMotionIsActive();
+        public static bool IsConnected { get; private set; } = false;
+
+        public static event Action OnConnected;
+        public static event Action OnDisconnected;
+        public static event Action<HeadphoneMotionData> OnUpdated;
+
+        public static void Start()
         {
-            Default = 0,
-            Left = 1,
-            Right = 2,
+            _unityHeadphoneMotionSetEventCallback(OnMotionEvent);
+            _unityHeadphoneMotionStart(OnMotionUpdate);
         }
 
-        public struct HeadphoneMotionData
+        public static void Stop()
         {
-            public double3 userAcceleration;
-            public double4 rotation;
-            public SensorLocation location;
+            _unityHeadphoneMotionStop();
         }
 
-        public static bool IsAvailable => false;
-        public static bool IsActive => false;
-        public static bool IsConnected => false;
-
-        public event Action OnConnected;
-        public event Action OnDisconnected;
-        public event Action<HeadphoneMotionData> OnUpdated;
-
-        public void Start()
+        [AOT.MonoPInvokeCallback(typeof(HeadphoneMotionDelegate))]
+        private static void OnMotionUpdate(HeadphoneMotionData motionData)
         {
-
+            Debug.Log("OnMotionUpdate");
+            if (OnUpdated != null)
+            {
+                OnUpdated(motionData);
+            }
         }
 
-        public void Stop()
+        [AOT.MonoPInvokeCallback(typeof(HeadphoneMotionEventDelegate))]
+        private static void OnMotionEvent(bool connected)
         {
-
+            Debug.Log("OnMotionEvent: " + (connected ? "Connected" : "Disconneted"));
+            IsConnected = connected;
+            if (OnConnected != null)
+            {
+                OnConnected();
+            }
+            if (OnDisconnected != null)
+            {
+                OnDisconnected();
+            }
         }
+
+        #region DllImport
+
+#if UNITY_IPHONE && !UNITY_EDITOR
+        [DllImport("__Internal")]
+        private static extern bool _unityHeadphoneDeviceMotionIsAvailable();
+        [DllImport("__Internal")]
+        private static extern bool _unityHeadphoneDeviceMotionIsActive();
+        [DllImport("__Internal")]
+        private static extern void _unityHeadphoneMotionStart(HeadphoneMotionDelegate callback);
+        [DllImport("__Internal")]
+        private static extern void _unityHeadphoneMotionStop();
+        [DllImport("__Internal")]
+        private static extern void _unityHeadphoneMotionSetEventCallback(HeadphoneMotionEventDelegate callback);
+#else
+        private static bool _unityHeadphoneDeviceMotionIsAvailable() => false;
+        private static bool _unityHeadphoneDeviceMotionIsActive() => false;
+        private static void _unityHeadphoneMotionStart(HeadphoneMotionDelegate callback) { }
+        private static void _unityHeadphoneMotionStop() { }
+        private static void _unityHeadphoneMotionSetEventCallback(HeadphoneMotionEventDelegate callback) { }
+#endif
+
+        #endregion // DllImport
     }
 }
